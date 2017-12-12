74e5b8906b409a95a68414ec61b77a02b5c2e1e7
Krzysztof Pobiarzyn <k.pobiarzyn@nomtek.com>
2017-12-12 22:34:14 +0100

    add authentication

--- app/src/main/java/pl/kpob/dietdiary/MainActivity.kt
+++ app/src/main/java/pl/kpob/dietdiary/MainActivity.kt
@@ -11,10 +11,18 @@ import pl.kpob.dietdiary.firebase.valueEventListener
 import pl.kpob.dietdiary.screens.MainScreen
 import pl.kpob.dietdiary.server.FbIngredient
 import pl.kpob.dietdiary.server.FbMeal
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 class MainActivity : SingleActivity(), AnkoLogger {
 
     private val fbSaver: FirebaseSaver by lazy { FirebaseSaver() }
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+
 
     private val ingredientsListener = valueEventListener {
 
@@ -48,8 +56,20 @@ class MainActivity : SingleActivity(), AnkoLogger {
         setContentView(R.layout.activity_main)
         initToken()
 
-        firebaseDb.ingredientsRef.addValueEventListener(ingredientsListener)
-        firebaseDb.mealsRef.addValueEventListener(mealsListener)
+        val credentials = credentials
+        auth
+                .signInWithEmailAndPassword(credentials.login, credentials.password)
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        info { it.result }
+                        firebaseDb.ingredientsRef.addValueEventListener(ingredientsListener)
+                        firebaseDb.mealsRef.addValueEventListener(mealsListener)
+                    } else {
+                        info { "ex ${it.exception}" }
+                        toast("Nie można się zalogować")
+                    }
+                }
+
     }
 
     private fun addPredefinedData() {
@@ -61,6 +81,7 @@ class MainActivity : SingleActivity(), AnkoLogger {
     override fun onDestroy() {
         firebaseDb.ingredientsRef.removeEventListener(ingredientsListener)
         firebaseDb.mealsRef.removeEventListener(mealsListener)
+        auth.signOut()
         super.onDestroy()
     }
 
@@ -78,4 +99,12 @@ class MainActivity : SingleActivity(), AnkoLogger {
 
     }
 
+    private val credentials: Credentials get() {
+        val file = assets.open("credentials")
+        return BufferedReader(InputStreamReader(file, "UTF-8")).use {
+            it.readLine().split(",").let { Credentials(it[0], it[1]) }
+        }
+    }
+
+    data class Credentials(val login: String, val password: String)
 }
